2cbe57edc0d33211c38a13911d28bfde2c52b2ee
nikolinacarevic <92435192+nikolinacarevic@users.noreply.github.com>
2021-12-21 18:29:36 +0100

    Update main9.c

--- vjezba9/main9.c
+++ vjezba9/main9.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 typedef struct Cvor* Stablo;
 struct Cvor {
@@ -11,9 +12,62 @@ struct Cvor {
 
 Stablo Insert(Stablo trenutni, Stablo NoviEl);
 Stablo StvoriNoviElement(int x);
+int Zamjeni(Stablo trenutni);
+int Ispis(Stablo S);
 
 int main()
 {
+	time_t t;
+	int odabir, br;
+	Stablo B = NULL;
+
+	srand((unsigned)time(&t));
+
+	while (1)
+	{
+		printf("\nOdaberite:\n1-Unos\n2-Zamjena\n3-Random\n4-Ispis u datoteku\n5-Kraj\n");
+		if (!scanf("%d", &odabir)) printf("Greska!");
+		else
+		{
+			switch (odabir)
+			{
+			case 1:
+			{
+				printf("Unesite novi element: ");
+				if (!scanf("%d", &br)) printf("Pogreška kod unosa novog elementa!\n");
+				else {
+					Stablo Novi = StvoriNoviElement(br);
+					B = Insert(B,Novi);
+				}
+				break;
+			}
+			case 2:
+			{
+			
+				break;
+			}
+			case 3:
+			{
+				
+				break;
+			}
+			case 4:
+			{
+				
+				break;
+			}
+			case 5:
+			{
+				return 0;
+			}
+			default:
+			{
+				printf("Pogreška u odabiru!\n");
+				break;
+			}
+			}
+		}
+	}
 
 	return 0;
 }
@@ -41,4 +95,19 @@ Stablo StvoriNoviElement(int x)
 	}
 	return NULL;
 
+}
+
+int Zamjeni(Stablo trenutni)
+{
+
+	return 0;
+}
+
+int Ispis(Stablo S)
+{
+	if (S == NULL) return 0;
+	Inorder(S->lijevo);
+	printf("%d ", S->broj);
+	Inorder(S->desno);
+	return 0;
 }
